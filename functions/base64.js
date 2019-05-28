1ab84f4a32e4cb8611e2c132ad3ebfb546b3bbc3
Daniel Thomas <Daniel.Thomas@Weaveability.com>
2019-05-28 15:37:14 +0100

    added the base64.toString method

---
+++ functions/base64.js
@@ -0,0 +1,26 @@
+const base64 = {
+
+  toString: (number) => {
+    const base64Chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz+/';
+    let base64String = [];
+
+    if (number === 0) return '0';
+
+    while (number > 0) {
+
+      const nextChar = number % 64;
+
+      number = (number - nextChar) / 64;
+
+      base64String.unshift(base64Chars[nextChar]);
+    
+    }
+
+
+
+    return base64String.join('');
+  }
+
+}
+
+module.exports = base64;
---
+++ tests/base64.test.js
@@ -0,0 +1,24 @@
+const base64 = require('../functions/base64');
+
+describe('base64.toString()', () => {
+  test('is a function', () => {
+    expect(base64.toString).toBeInstanceOf(Function)
+  });
+  test('for values 0 - 63 returns appropriate character', () => {
+    expect(base64.toString(0)).toBe('0')
+    expect(base64.toString(1)).toBe('1')
+    expect(base64.toString(10)).toBe('A')
+    expect(base64.toString(16)).toBe('G')
+    expect(base64.toString(60)).toBe('y')
+    expect(base64.toString(62)).toBe('+')
+    expect(base64.toString(63)).toBe('/')
+  });
+  test('for values 63+ returns appropriate string', () => {
+    expect(base64.toString((64 * 1) + 0)).toBe('10');
+    expect(base64.toString((64 * 1) + 32)).toBe('1W');
+    expect(base64.toString((64 * 10) + 10)).toBe('AA');
+    expect(base64.toString((64 * 63) + 63)).toBe('//');
+    expect(base64.toString((64 * 64 * 5) + (64 * 2) + 7)).toBe('527');
+    expect(base64.toString((64 * 64 * 59) + (64 * 11) + 17)).toBe('xBH');
+  });
+});
