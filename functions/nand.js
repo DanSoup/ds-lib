08bab319a2cf95ee5541eb30dd5c567d417997c4
Daniel Thomas <Daniel.Thomas@Weaveability.com>
2019-05-30 11:28:04 +0100

    refactored nand

--- functions/nand.js
+++ functions/nand.js
@@ -1,9 +1,8 @@
 const nand = (a, b) => {
 
-  if (!a && !b) return true;
-  if (a && !b) return true;
-  if (!a && b) return true;
-  if (a && b) return false;
+  if (!b) return true;
+  else if (!a) return true;
+  else return false;
 
 };
 
