b8363922d3bf57c374a4b4bf67cbcd1f994ed150
Daniel Thomas <Daniel.Thomas@Weaveability.com>
2019-06-06 11:36:25 +0100

    fixed typo

--- functions/rand.js
+++ functions/rand.js
@@ -5,7 +5,7 @@
   rand() - Generates a random number from 0 to 1. Equivalent to Math.random().
   rand( $1:Number ) - Generates a random number from 0 to $1.
   rand( $1:Number, $2:Number ) - Generates a random number from $1 to $2.
-  rand( $1:Number, $2:Number , $3:Number ) - Generates a random from $1 to $2 in steps of $3.
+  rand( $1:Number, $2:Number , $3:Number ) - Generates a random number from $1 to $2 in steps of $3.
   rand( $1:Array ) - Selects a random element from $1. 
 */
 
